803a35bd4a233ca75877eb65d8a26fa53c847561
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-08 00:28:56 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 // Functions Javascript
 
-function sum(x, y) {
-  var total = x + y;
+// function sum(x, y) {
+//   var total = x + y;
 
-  document.write(total);
-}
+//   document.write(total);
+// }
 
-sum(15, 2);
+// sum(15, 2);
 
 // can't use variables defined inside a function
 
 // document.write(total);
+
+function sum(x, y) {
+  var total = x + y;
+  // use return to make it globe not local inside the function
+  return total;
+}
+
+sum('Hello ', 'I am Matthew.');
+document.write(total);
